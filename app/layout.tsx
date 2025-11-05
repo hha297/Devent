f13c9e96aec8cbebdbc7f852f166b63c66dc82e1
hoangha297 <ha.dh290702@gmail.com>
2025-11-05 10:49:57 +0200

    Setup lightray & components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-primary",
@@ -19,7 +20,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.variable} antialiased`}>{children}</body>
+      <body className={`${spaceGrotesk.variable} antialiased min-h-screen`}>
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5DFECA"
+            raysSpeed={0.5}
+            lightSpread={1}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0.01}
+          />
+        </div>
+
+        <main>{children}</main>
+      </body>
     </html>
   );
 }

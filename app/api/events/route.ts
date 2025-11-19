30278d17b1640fbba6b5531c3b4dfb0823f8e76d
hoangha297 <ha.dh290702@gmail.com>
2025-11-19 10:23:37 +0200

    Image Upload

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/mongodb';
 import { Event } from '@/database';
-
+import { v2 as cloudinary } from 'cloudinary';
 export async function POST(req: NextRequest) {
         try {
                 await connectDB();
@@ -14,6 +14,31 @@ export async function POST(req: NextRequest) {
                         console.error(error);
                         return NextResponse.json({ message: 'Invalid form data' }, { status: 400 });
                 }
+
+                const file = formData.get('image') as File;
+                if (!file) {
+                        return NextResponse.json({ message: 'Image is required' }, { status: 400 });
+                }
+                const arrayBuffer = await file.arrayBuffer();
+
+                const buffer = Buffer.from(arrayBuffer);
+
+                const uploadResult = await new Promise((resolve, reject) => {
+                        cloudinary.uploader
+                                .upload_stream(
+                                        {
+                                                resource_type: 'image',
+                                                folder: 'Devent',
+                                        },
+                                        (error, result) => {
+                                                if (error) reject(error);
+                                                resolve(result);
+                                        },
+                                )
+                                .end(buffer);
+                });
+
+                event.image = (uploadResult as { secure_url: string }).secure_url;
 
                 const createdEvent = await Event.create(event);
                 return NextResponse.json(
@@ -29,5 +54,16 @@ export async function POST(req: NextRequest) {
                         },
                         { status: 500 },
                 );
+        }
+}
+
+export async function GET(req: NextRequest) {
+        try {
+                await connectDB();
+                const events = await Event.find().sort({ createdAt: -1 });
+                return NextResponse.json({ events }, { status: 200 });
+        } catch (error) {
+                console.error(error);
+                return NextResponse.json({ message: 'Failed to fetch events' }, { status: 500 });
         }
 }
